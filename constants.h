addb80a9fa4115fa9ea9d64bf772ac74517ca674
Miroslav Balev <zevs8@abv.bg>
2024-12-25 22:15:01 +0200

    Add global constants

---
+++ constants.h
@@ -0,0 +1,7 @@
+#pragma once
+
+constexpr int DEFAULT_COLOR = 0;
+
+constexpr int DEFAULT_HEADER_LEFT_MARGIN = 3;
+constexpr int DEFAULT_DIVIDER_LENGTH = 25;
+constexpr char DEFAULT_DIVIDER_CHAR = '-';
